556edf0080482aad624cba2b735ccd00db119477
Josebratt <josebratt@gmail.com>
2021-11-17 17:40:50 -0500

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { AuthService } from '../core/auth.service';
 import { userProfile } from '../core/user-profile.model';
 
 @Component({
@@ -17,9 +18,11 @@ export class ProfileComponent implements OnInit {
   constructor(public afAuth: AngularFireAuth, public afs: AngularFirestore) { 
   }
 
-  ngOnInit(): void {
-      // this.itemDoc = this.afs.doc<userProfile>(`users/${this.afAuth.currentUser}`);
-      // this.item = this.itemDoc.valueChanges();
+  async ngOnInit() {
+
+       this.itemDoc = this.afs.doc<userProfile>(`users/${this.afAuth.currentUser}`);
+      //  this.item = this.itemDoc.valueChanges();
   }
+
 
 }
