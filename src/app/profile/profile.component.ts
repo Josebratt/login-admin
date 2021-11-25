d12815f19ef57e494c0adc824e40f4e2c3a49db4
Josebratt <josebratt@gmail.com>
2021-11-25 17:39:35 -0500

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from '../core/auth.service';
 import { userProfile } from '../core/user-profile.model';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -13,19 +15,63 @@ import { userProfile } from '../core/user-profile.model';
 export class ProfileComponent implements OnInit {
 
   private itemDoc: AngularFirestoreDocument<userProfile> | undefined;
-  item!: Observable<userProfile>;
-  selectedValue: any;
+  item!: Observable<userProfile | undefined>;
+  uid: string | null;
 
-  constructor(public afAuth: AngularFireAuth, public afs: AngularFirestore) { 
+  loading = false;
+  error: string = '';
+
+  constructor(
+      public afAuth: AngularFireAuth, 
+      public afs: AngularFirestore, 
+      private route: ActivatedRoute, 
+      private auth: AuthService) { 
+      this.uid = this.route.snapshot.paramMap.get('id');
   }
 
   ngOnInit() {
+      //  this.itemDoc = this.afs.doc<userProfile>(`users/${this.afAuth.currentUser}`);
+      this.itemDoc = this.afs.doc<userProfile>(`users/${this.uid}`);
+      this.item = this.itemDoc.valueChanges();
+  }
 
-    // this.afs.doc<userProfile>(`users/${this.afAuth.currentUser.then(
-    //   data => this.itemDoc = data
-    // )}`)
-       this.itemDoc = this.afs.doc<userProfile>(`users/${this.afAuth.currentUser}`);
-      //  this.item = this.itemDoc.valueChanges();
+  async onSubmit(ngForm: NgForm){
+      this.loading = true;
+
+      const {
+        name,
+        email,
+        address,
+        city,
+        state,
+        zip,
+        phone,
+        specialty,
+        ip,
+    } = ngForm.form.getRawValue();
+
+    // Create UserProfile object  
+    const userProfile: userProfile = {
+        uid: this.uid!,
+        name,
+        email,
+        address,
+        city,
+        state,
+        zip,
+        phone,
+        specialty,
+        ip
+    };
+
+    try {
+      await this.auth.updateUserDocument(userProfile);
+    } catch (error: any){
+      console.log(error.message);
+      this.error = error.message;
+    }
+
+    this.loading = false;
   }
 
 }
