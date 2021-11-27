b22d501bbe297f2c3dfda46c61c80ba5091d8f75
Josebratt <josebratt@gmail.com>
2021-11-27 13:16:59 -0500

    Update users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { userProfile } from '../core/user-profile.model';
 
 @Component({
   selector: 'app-users',
@@ -7,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  private usersCollection: AngularFirestoreCollection<userProfile>;
+  users: Observable<userProfile[]>;
+
+  constructor(private afs: AngularFirestore) { 
+    this.usersCollection = afs.collection<userProfile>('users');
+    this.users = this.usersCollection.valueChanges();
+  }
 
   ngOnInit(): void {
   }
